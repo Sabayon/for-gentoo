05d8b674a30dba537e546f365da8859ebad0101b
Danilo Pianini <danilo.pianini@gmail.com>
2012-03-23 00:32:43 +0100

    [www-misc/zoneminder] Version from dilfridge overlay

--- www-misc/zoneminder/files/init.d
+++ www-misc/zoneminder/files/init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/www-misc/zoneminder/files/init.d,v 1.3 2009/01/01 16:47:24 gentoofan23 Exp $
+# $Header: /var/cvsroot/gentoo-x86/www-misc/zoneminder/files/Attic/init.d,v 1.3 2009/01/01 16:47:24 gentoofan23 Exp $
 
 depend() {
 	use mysql
